73e64dcadb035a0b4e7fb63628644424330823d3
Kode-n-Rolla <pavlucio.auditore@gmail.com>
2026-04-11 13:25:02 -0300

    Phase 1 done. Implement fetch logic

--- src/cli.rs
+++ src/cli.rs
@@ -1,14 +1,68 @@
-/*
-    Greeting line with `shuka` naming
-    flags:
-        --explorer (eth, battlechain)
-        --address (0x...)
-        --chainid
-        --out (-o) -> (no neccessary flag) save to certain dir
-*/
+use std::path::PathBuf;
 
-use crate::error::ShukaError;
+use crate::{error::ShukaError,
+            types::{ExplorerKind, FetchRequest},
+};
+use clap::{Parser, Subcommand, Args, ValueEnum};
+
+#[derive(Debug, Parser)]
+#[command(name = "shuka")]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    #[command(override_usage = "shuka fetch <EXPLORER> <ADDRESS> <CHAIN_ID> [OPTIONS]")]
+    Fetch(FetchArgs),
+}
+
+#[derive(Debug, Args)]
+pub struct FetchArgs {
+    pub explorer: CliExplorer,
+    pub address: String,
+    pub chain_id: u32,
+    #[arg(short, long)]
+    pub out: Option<PathBuf>,
+}
+
+impl FetchArgs {
+    pub fn into_request(self) -> FetchRequest {
+        let request: FetchRequest = FetchRequest { 
+            explorer: self.explorer.into(),
+            address: self.address,
+            chain_id: self.chain_id,
+            output_dir: self.out,
+        };
+
+        request
+    }
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+pub enum CliExplorer {
+    Battlechain,
+    Ethereum,
+}
+
+impl From<CliExplorer> for ExplorerKind {
+    fn from(value: CliExplorer) -> Self {
+        match value {
+            CliExplorer::Battlechain => ExplorerKind::Battlechain,
+            CliExplorer::Ethereum => ExplorerKind::Ethereum,
+        }
+    }
+}
 
 pub fn run() -> Result<(), ShukaError> {
-    todo!()
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Fetch(args) => {
+            let request = args.into_request();
+            println!("Request: {:#?}", request);
+            Ok(())
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use shuka::cli;
 
 fn main() -> Result<(), shuka::error::ShukaError> {
+    println!("shuka greetings you\n");
     cli::run()
 }
