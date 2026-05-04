b0bf06162a3aa46d0c687655ee3f6f2a38b94ced
Kode-n-Rolla <pavlucio.auditore@gmail.com>
2026-05-04 18:05:30 -0300

    Add parser support for single and multi-file Etherscan soursec

--- src/parsers/source.rs
+++ src/parsers/source.rs
@@ -25,39 +25,78 @@ pub fn parse_source(source: &RawExplorerResponse) -> Result<ParsedSourceBundle, 
         .first()
         .ok_or_else(|| ShukaError::Parser("result array is empty".to_string()))?;
 
-    // Get the `SourceCode`` field
+    // Get the `SourceCode` field for 1 contract
     let source_code = contract_entry
         .get("SourceCode")
         .ok_or_else(|| ShukaError::Parser("`SourceCode` field is empty".to_string()))?
         .as_str()
         .ok_or_else(|| ShukaError::Parser("`SourceCode` is not a string".to_string()))?;
-
-    // Get `ContractName` field
+    
+        // Get `ContractName` field
     let contract_name = contract_entry
         .get("ContractName")
         .ok_or_else(|| ShukaError::Parser("`ContractName` field is empty".to_string()))?
         .as_str()
         .ok_or_else(|| ShukaError::Parser("`ContractName` is not a string".to_string()))?;
-
+    
     // Get `CompilerVersion` field
     let compiler_version = contract_entry
         .get("CompilerVersion")
-        .ok_or_else(|| ShukaError::Parser("`CompilerVersion` filed is empty".to_string()))?
+        .ok_or_else(|| ShukaError::Parser("`CompilerVersion` field is empty".to_string()))?
         .as_str()
         .ok_or_else(|| ShukaError::Parser("`CompilerVersion` is not a string".to_string()))?;
 
-    println!("{}", source_code);
+    let trimmed_source_code = source_code.trim();
+
+    // Get the `SourceCode` field for multi contracts
+    let files = if trimmed_source_code.starts_with("{") {
+        let parsed_multi_contracts: Value = serde_json::from_str(&trimmed_source_code)
+            .map_err(|err| ShukaError::Parser(format!("failed to parse multi contracts: {err}")))?;
+
+        let file_map = if let Some(sources_value) = parsed_multi_contracts.get("sources") {
+            sources_value
+                .as_object()
+                .ok_or_else(|| ShukaError::Parser("sources field is not an object".to_string()))?
+        } else {
+            parsed_multi_contracts
+                .as_object()
+                .ok_or_else(|| ShukaError::Parser("structured SourceCode file map not found".to_string()))?
+        };
+
+        let mut contracts = Vec::new();
+        for (file_name, file_value) in file_map {
+            let content = file_value
+                .get("content")
+                .ok_or_else(|| ShukaError::Parser("file content field is missing".to_string()))?
+                .as_str()
+                .ok_or_else(|| ShukaError::Parser("file content is not a string".to_string()))?;
+            
+            contracts.push(SourceFile {
+                path: file_name.into(),
+                content: content.into(),
+            });
+        }
+
+        contracts
+    } else {
+        // single contract push
+        let contract = vec![
+            SourceFile {
+                path: format!("{}.sol", contract_name).into(),
+                content: source_code.into(),
+        }];
+        contract
+    };
+
+    println!("{:?}", files[0].content);
 
     Ok(ParsedSourceBundle {
-        files: vec![
-            SourceFile {
-                path: format!("{}.sol", contract_name).into(), // @todo think about few contracts
-                content: source_code.into(),
-        },
-        ],
+        files,
         metadata: Some(ContractMetadata {
             contract_name: contract_name.into(),
             compiler_version: compiler_version.into(),
         }),
     })
 }
+
+// @todo move functionality to separate function (func per explorer) when implementing `battlechain` explorer
