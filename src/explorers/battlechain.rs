c7c83c2dd2c78afd7ddc9954e50b769600e8d406
Kode-n-Rolla <pavlucio.auditore@gmail.com>
2026-05-18 17:52:13 -0300

    Add Battlechain explorer fetch support

--- src/explorers/battlechain.rs
+++ src/explorers/battlechain.rs
@@ -4,12 +4,35 @@ use crate::{
     types::{FetchRequest, RawExplorerResponse},
 };
 
+use reqwest::blocking;
+
+const BASE_API_URL: &str = "https://block-explorer-api.testnet.battlechain.com/api";
+
 pub struct BattlechainExplorer;
 
 impl SourceExplorer for BattlechainExplorer {
-    fn fetch(&self, _request: &FetchRequest) -> Result<RawExplorerResponse, ShukaError> {
+    fn fetch(&self, request: &FetchRequest) -> Result<RawExplorerResponse, ShukaError> {
+        let client = blocking::Client::new();
+
+        let response = client.get(BASE_API_URL)
+            .query(&[
+                ("module", "contract".to_string()),
+                ("action", "getsourcecode".to_string()),
+                ("address", request.address.clone())
+            ])
+            .send()
+            .map_err(|err| ShukaError::Explorer(format!("failed to send request to Battlechain: {err}")))?;
+
+        let status = response.status();
+        if !status.is_success() {
+            return Err(ShukaError::Explorer(format!("Battlechain API returned unsuccessful status: {status}")));
+        }
+
+        let body = response.text()
+            .map_err(| err | ShukaError::Explorer(format!("Failed to handle body: {err}")))?;
+
         Ok(RawExplorerResponse {
-            body: "Battlechain response".to_string()
-        })
+            body,
+         })
     }
 }
